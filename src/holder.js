32ee2d3438d801d8fef946265b0dbb2a0f7aad6f
Benjamin Ugbene <benjamin.ugbene@worldfirst.com>
2017-08-12 16:44:37 +0100

    upgrade fixes

--- src/holder.js
+++ src/holder.js
@@ -1,10 +1,10 @@
 // vendor modules
 
-import holderjs from 'holderjs'
-import { omit, pick } from 'lodash'
-import { stringify } from 'qs'
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import holderjs from 'holderjs';
+import { omit, pick } from 'lodash';
+import { stringify } from 'qs';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 // private variables
 
@@ -18,16 +18,16 @@ const HOLDER_ATTR_NAMES = [
   'font',
   'align',
   'outline',
-  'lineWrap',
-]
+  'lineWrap'
+];
 
-const REGEX_PERCENTAGE = /\d+(%|p)/
+const REGEX_PERCENTAGE = /\d+(%|p)/;
 
-const REGEX_PX = /\d+px%/
+const REGEX_PX = /\d+px%/;
 
-const REGEX_NUMBER = /\d+/
+const REGEX_NUMBER = /\d+/;
 
-const SHOULD_UPDATE_ON_RESIZE_DEFAULT = true
+const SHOULD_UPDATE_ON_RESIZE_DEFAULT = true;
 
 // private functions
 
@@ -39,30 +39,30 @@ const SHOULD_UPDATE_ON_RESIZE_DEFAULT = true
  * @return {string}
  */
 function sanitizeNumber (numberString) {
-  return String(Number.parseInt(String(numberString), 10))
+  return String(Number.parseInt(String(numberString), 10));
 }
 
 function sanitizeSizeValue (size) {
-  let stringSize = size
+  let stringSize = size;
   if (typeof stringSize !== 'string') {
-    stringSize = String(stringSize)
+    stringSize = String(stringSize);
   }
 
   if (REGEX_PERCENTAGE.test(stringSize)) {
-    return `${sanitizeNumber(stringSize)}p`
+    return `${sanitizeNumber(stringSize)}p`;
   }
 
   if (REGEX_PX.test(stringSize)) {
-    return sanitizeNumber(stringSize)
+    return sanitizeNumber(stringSize);
   }
 
   if (REGEX_NUMBER.test(stringSize)) {
-    return stringSize
+    return stringSize;
   }
 
-  console.warn(`holder-js size not valid: ${size}, defaulting to 100%`)
+  console.warn(`holder-js size not valid: ${size}, defaulting to 100%`);
 
-  return '100p'
+  return '100p';
 }
 
 function createPlaceholder (node, updateOnResize = SHOULD_UPDATE_ON_RESIZE_DEFAULT) {
@@ -71,63 +71,67 @@ function createPlaceholder (node, updateOnResize = SHOULD_UPDATE_ON_RESIZE_DEFAU
     images: node,
     object: null,
     bgnodes: null,
-    stylenodes: null,
-  })
+    stylenodes: null
+  });
   if (updateOnResize === false) {
-    holderjs.setResizeUpdate(node, false)
+    holderjs.setResizeUpdate(node, false);
   }
 }
 
 function updatePlaceholder (node, updateOnResize = SHOULD_UPDATE_ON_RESIZE_DEFAULT) {
-  holderjs.setResizeUpdate(node, Boolean(updateOnResize))
+  holderjs.setResizeUpdate(node, Boolean(updateOnResize));
 }
 
 // exports
 
 class Holder extends Component {
+  static get propTypes () {
+    return {
+      width: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ]).isRequired,
+      height: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+      ]).isRequired,
+      updateOnResize: PropTypes.bool,
+      alt: PropTypes.string
+    };
+  }
 
-  render () {
-    const { width, height } = this.props
-    const holderConfig = pick(this.props, HOLDER_ATTR_NAMES)
-    const htmlAttrs = omit(this.props, HOLDER_ATTR_NAMES)
-
-    const src = `holder.js/${sanitizeSizeValue(width)}x${sanitizeSizeValue(height)}?${stringify(holderConfig)}`
-    return (
-      <img
-        ref='placeholder'
-        data-src={src}
-        {...htmlAttrs}
-        />
-    )
+  static get defaultProps () {
+    return {
+      width: '100',
+      height: '100',
+      updateOnResize: false,
+      alt: ''
+    };
   }
 
   componentDidMount () {
-    const placeholderNode = ReactDOM.findDOMNode(this.refs.placeholder)
-    createPlaceholder(placeholderNode, this.props.updateOnResize)
+    createPlaceholder(this.placeholder, this.props.updateOnResize);
   }
 
   componentDidUpdate () {
-    const placeholderNode = ReactDOM.findDOMNode(this.refs.placeholder)
-    updatePlaceholder(placeholderNode, this.props.updateOnResize)
+    updatePlaceholder(this.placeholder, this.props.updateOnResize);
   }
 
+  render () {
+    const { width, height, alt } = this.props;
+    const holderConfig = pick(this.props, HOLDER_ATTR_NAMES);
+    const htmlAttrs = omit(this.props, HOLDER_ATTR_NAMES);
+    const src = `holder.js/${sanitizeSizeValue(width)}x${sanitizeSizeValue(height)}?${stringify(holderConfig)}`;
+
+    return (
+      <img
+        ref={(node) => { this.placeholder = node; }}
+        data-src={src}
+        alt={alt}
+        {...htmlAttrs}
+      />
+    );
+  }
 }
 
-Holder.propTypes = {
-  width: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.number,
-  ]).isRequired,
-  height: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.number,
-  ]).isRequired,
-  updateOnResize: React.PropTypes.bool,
-}
-
-Holder.defaultProps = {
-  width: '100',
-  height: '100',
-}
-
-export default Holder
+export default Holder;
